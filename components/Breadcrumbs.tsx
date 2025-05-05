3ce7afeccf96c66a84482b5bcd40edfada8a4893
Gabriel Rodrigues Magalhães <grm@cesar.school>
2025-05-05 18:34:41 -0300

    fix: doc breadcrumbs
    -Alterado redirecionamento do breadcrumbs

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -22,22 +22,26 @@ function Breadcrumbs() {
         </BreadcrumbItem>
 
         {segments.map((segment, index) => {
-            if (!segment) return null;
+          if (!segment) return null;
+          const href =
+            segment === "doc"
+              ? "/"
+              : `/${segments.slice(0, index + 1).join("/")}`;
 
-            const href = `/${segments.slice(0, index + 1).join("/")}`;
-            const isLast = index === segments.length -1;
-            return (
-              <Fragment key={segment}>
-                <BreadcrumbSeparator/>
-                <BreadcrumbItem>
+          const isLast = index === segments.length - 1;
+
+          return (
+            <Fragment key={segment}>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
                 {isLast ? (
-                  <BreadcrumbPage>{segment}</BreadcrumbPage>  
-                ): (
+                  <BreadcrumbPage>{segment}</BreadcrumbPage>
+                ) : (
                   <BreadcrumbLink href={href}>{segment}</BreadcrumbLink>
                 )}
-                </BreadcrumbItem>
-              </Fragment>
-            )
+              </BreadcrumbItem>
+            </Fragment>
+          );
         })}
         
       </BreadcrumbList>
